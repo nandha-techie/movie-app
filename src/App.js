25dd095b6a53d9a87a7117ae15d12d3200c5b24d
nandha-techie <nandhawebapps@gmail.com>
2023-02-14 12:57:47 +0530

    api bug fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ function App() {
   }, []);
   
   const searchMovies = async (title)=> {
-    const header = {"Access-Control-Allow-Origin" : "*", "Access-Control-Allow-Credentials" : true };
+    const header = { method: 'GET',
+      headers: {
+        accept: 'application/json'}};
       const response = await fetch(`${API_URL}&s=${title}`, header);
       const data = await response.json();
       setMovies(data.Search);
